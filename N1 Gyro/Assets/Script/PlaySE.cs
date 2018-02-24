8a000c86f6d25896bb275d6960d4c8c9f1f3b752
sunkenship <moltke0316@gmail.com>
2018-02-24 19:59:02 +0900

    a

---
+++ N1 Gyro/Assets/Script/PlaySE.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySE : MonoBehaviour {
+
+    public AudioClip gameStart;
+    public AudioClip Cllide;
+}
